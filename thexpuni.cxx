1d2290e288716a8143619f6b2be7e7125cb10295
Vladimir Georgiev <vld.georgiev@gmail.com>
2016-06-06 13:42:30 +0300

    Better alignment of entrance icon in KML to point to exact location

--- thexpuni.cxx
+++ thexpuni.cxx
@@ -470,11 +470,11 @@ void thexpmap::export_kml(class thdb2dxm * maps, class thdb2dprj * prj)
   thscrap * scrap;
   thexpuni xu;
 
-  fprintf(out,"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<kml xmlns=\"http://earth.google.com/kml/2.0\">\n");
-  fprintf(out,"<Folder>\n");
-  fprintf(out,"<Icon> <href>http://pk-sofia.com/images/stories/IconLine.png</href> </Icon>\n");
-  fprintf(out,"<Style id=\"ThMapStyle\"> <PolyStyle> <fill>1</fill> <outline>0</outline> </PolyStyle> </Style>\n");
-  fprintf(out,"<Style id=\"ThEntranceIcon\"> <IconStyle> <Icon> <href>http://pk-sofia.com/images/stories/IconEntrance.png</href> </Icon> </IconStyle> </Style>\n");
+  fprintf(out, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<kml xmlns=\"http://earth.google.com/kml/2.0\">\n");
+  fprintf(out, "<Folder>\n");
+  fprintf(out, "<Icon> <href>http://pk-sofia.com/images/stories/IconLine.png</href> </Icon>\n");
+  fprintf(out, "<Style id=\"ThMapStyle\"> <PolyStyle> <fill>1</fill> <outline>0</outline> </PolyStyle> </Style>\n");
+  fprintf(out, "<Style id=\"ThEntranceIcon\"> <IconStyle> <Icon> <href>http://pk-sofia.com/images/stories/IconEntrance.png</href> </Icon> <hotSpot x=\"0.5\" y=\"0.5\" xunits=\"fraction\" yunits=\"fraction\"> </IconStyle> </Style>\n");
   // VG 250616: TODO change icon above, maybe upload to therion website after testing
 
   thsurvey * mainsrv = db->fsurveyptr;
