a93c8d804b8514ed00b496b490983ba693873b4d
Rodrigo Severo <rsev@protonmail.com>
2019-12-15 17:41:32 -0300

    New EGB point: bat

--- thsymbolset.cxx
+++ thsymbolset.cxx
@@ -420,6 +420,7 @@ int thsymbolset__get_id(const char * symclass, const char * symbol)
         cp3(TT_POINT_TYPE_ALTAR,SYMP_ALTAR);
         cp3(TT_POINT_TYPE_ARCHEOEXCAVATION,SYMP_ARCHEOEXCAVATION);
         cp3(TT_POINT_TYPE_AUDIO,SYMP_AUDIO);
+        cp3(TT_POINT_TYPE_BAT,SYMP_BAT);
         cp3(TT_POINT_TYPE_DANGER,SYMP_DANGER);
         cp3(TT_POINT_TYPE_ELECTRICLIGHT,SYMP_ELECTRICLIGHT);
         cp3(TT_POINT_TYPE_EXVOTO,SYMP_EXVOTO);
@@ -1365,6 +1366,7 @@ void thsymbolset::export_pdf(class thlayout * layout, FILE * mpf, unsigned & sfi
   legend_point(SYMP_ALTAR,thT("point altar",layout->lang));
   legend_point(SYMP_ARCHEOEXCAVATION,thT("point archeo-excavation",layout->lang));
   legend_point(SYMP_AUDIO,thT("point audio",layout->lang));
+  legend_point(SYMP_BAT,thT("point bat",layout->lang));
   legend_point(SYMP_DANGER,thT("point danger",layout->lang));
   legend_point(SYMP_ELECTRICLIGHT,thT("point electric-light",layout->lang));
   legend_point(SYMP_EXVOTO,thT("point ex-voto",layout->lang));
