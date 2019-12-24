ef182741e8d17be63cef41efb90e2038edb91c7b
Rodrigo Severo <rsev@protonmail.com>
2019-12-24 07:23:02 -0300

    New EGB area: pillar-with-curtains

--- thsymbolset.cxx
+++ thsymbolset.cxx
@@ -254,6 +254,7 @@ int thsymbolset__get_id(const char * symclass, const char * symbol)
         c2(TT_AREA_TYPE_MUDCRACK, SYMA_MUDCRACK);
         c2(TT_AREA_TYPE_PEBBLES, SYMA_PEBBLES);
         c2(TT_AREA_TYPE_PILLAR, SYMA_PILLAR);
+        c2(TT_AREA_TYPE_PILLARWITHCURTAINS, SYMA_PILLARWITHCURTAINS);
         c2(TT_AREA_TYPE_SAND, SYMA_SAND);
         c2(TT_AREA_TYPE_SNOW, SYMA_SNOW);
         c2(TT_AREA_TYPE_STALACTITE, SYMA_STALACTITE);
@@ -725,6 +726,7 @@ int thsymbolset__get_group(int group_id, int cid) {
     group(38,SYMA_STALACTITESTALAGMITE);
     group(39,SYMA_STALACTITE);
     group(40,SYMA_PILLAR);
+    group(41,SYMA_PILLARWITHCURTAINS);
     egroup
 
     bgroup(SYMX_ICE)
@@ -1278,6 +1280,7 @@ void thsymbolset::export_pdf(class thlayout * layout, FILE * mpf, unsigned & sfi
   legend_area(SYMA_MUDCRACK,thT("area mudcrack",layout->lang));
   legend_area(SYMA_PEBBLES,thT("area pebbles",layout->lang));
   legend_area(SYMA_PILLAR,thT("area pillar",layout->lang));
+  legend_area(SYMA_PILLARWITHCURTAINS,thT("area pillar-with-curtains",layout->lang));
   legend_area(SYMA_SAND,thT("area sand",layout->lang));
   legend_area(SYMA_SNOW,thT("area snow",layout->lang));
   legend_area(SYMA_STALACTITE,thT("area stalactite",layout->lang));
