074b328df4f888b28ead46e89946956fa11b538d
mbudaj <m.budaj@gmail.com>
2020-04-04 19:19:01 +0200

    fix a test for proj v.4

--- utest-proj.cxx
+++ utest-proj.cxx
@@ -5,11 +5,7 @@
 
 // tests for coordinate systems transformations using Proj library
 
-#if PROJ_VER < 5
-double th_eps = 130;  // a quick fix to make test pass; +towgs84 seems to be ignored in proj v.4
-#else
 double th_eps = 0.5;
-#endif
 
 bool coord_equal(double x1, double x2) {
   return fabs(x1 - x2) < th_eps;
@@ -18,7 +14,7 @@ bool coord_equal(double x1, double x2) {
 TEST_CASE( "projections: towgs", "[proj]" ) {
     double x,y,z;
     thcs2cs("+proj=krovak +czech +ellps=bessel +towgs84=485.021,169.465,483.839,7.786342,4.397554,4.102655,0", 
-            "+proj=utm +zone=34",
+            "+proj=utm +zone=34 +ellps=WGS84 +datum=WGS84 +units=m",
             379033, 1208895, 2025.31, x, y, z);
     CHECK(coord_equal(x,401375.61));
     CHECK(coord_equal(y,5421243.27));
