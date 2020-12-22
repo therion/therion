b9191b4b790cc8fdce98af101b69dd717f833929
mbudaj <m.budaj@gmail.com>
2020-12-22 17:06:07 +0100

    print Proj version in unit tests

--- utest-main.cxx
+++ utest-main.cxx
@@ -1,10 +1,14 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch2/catch.hpp>
 #include "thinit.h"
+#include "thproj.h"
 
 int main( int argc, char* argv[] ) {
 
   thini.set_proj_lib_path();
+
+  printf("utest: using Proj %s, compiled against %s\n", thcs_get_proj_version().c_str(),
+                                                        thcs_get_proj_version_headers().c_str());
 
   int result = Catch::Session().run( argc, argv );
 
