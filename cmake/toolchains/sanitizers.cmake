190c866c9f43f291129bee5fcadaffd10bd08c6a
Afforix <plch.matej@gmail.com>
2021-03-03 20:29:03 +0100

    changed deprecated flag

--- cmake/toolchains/sanitizers.cmake
+++ cmake/toolchains/sanitizers.cmake
@@ -1,7 +1,7 @@
 # Enable sanitizers.
 
 # Fail on errors.
-set(CMAKE_C_FLAGS_INIT "-fno-sanitize-recover")
-set(CMAKE_CXX_FLAGS_INIT "-fno-sanitize-recover")
+set(CMAKE_C_FLAGS_INIT "-fno-sanitize-recover=all")
+set(CMAKE_CXX_FLAGS_INIT "-fno-sanitize-recover=all")
 # ASan and UBSan can be run together.
 set(ECM_ENABLE_SANITIZERS "address;undefined" CACHE STRING "Enable runtime sanitizers, available options: address,memory,thread,leak,undefined." )
