96d41366a8702dabae4bdbacf3267b2a63f27681
Afforix <plch.matej@gmail.com>
2021-04-12 22:29:41 +0200

    qtherion fixes - cmake integration - native file dialog - fixed warnings

--- qtherion/main.cpp
+++ qtherion/main.cpp
@@ -1,4 +1,4 @@
-#include <QGuiApplication>
+#include <QApplication>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
 
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
-    QGuiApplication app(argc, argv);
+    QApplication app(argc, argv);
 
     QTherion qtherion;
 
--- qtherion/objectmodel.h
+++ qtherion/objectmodel.h
@@ -32,7 +32,7 @@ public:
 
     int rowCount(const QModelIndex &parent = QModelIndex()) const override;
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
-    QHash<int, QByteArray> roleNames() const;
+    QHash<int, QByteArray> roleNames() const override;
 
 };
 
