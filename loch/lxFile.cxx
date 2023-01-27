3213869d3e49fdcaf4186c8e958bb3173e0986ee
Stacho Mudrák <stacho.mudrak@gmail.com>
2023-01-27 15:44:59 +0100

    cmake fix

--- loch/lxFile.cxx
+++ loch/lxFile.cxx
@@ -10,7 +10,7 @@
 #include <list>
 #include <fstream>
 #include <filesystem>
-#include <fmt/printf.h>
+//#include <fmt/printf.h>
 
 #endif  
 //LXDEPCHECK - standard libraries
@@ -22,8 +22,12 @@
 namespace fs = std::filesystem;
 
 double lxFilePrepDbl(double val) {
-  std::string tb;
-  return atof(fmt::sprintf("%.12e", val).c_str());
+  //std::string tb;
+  //return atof(fmt::sprintf("%.12e", val).c_str());
+  char tb[32];
+  snprintf(tb, 31, "%.12e", val);
+  return atof(tb);
+
 }
 
 
