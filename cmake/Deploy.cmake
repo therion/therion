f7402b4666e0869eb988886c9db064fb724c02c0
Matěj Plch <plch.matej@gmail.com>
2024-11-19 20:02:29 +0100

    use GET_RUNTIME_DEPENDENCIES

--- cmake/Deploy.cmake
+++ cmake/Deploy.cmake
@@ -1,12 +1,12 @@
 # Find all DLL dependencies to deploy on Windows.
-include(GetPrerequisites)
-get_prerequisites(${THERION} DLLS 1 1 "" "")
-get_prerequisites(${LOCH}    DLLS 1 1 "" "")
+file(GET_RUNTIME_DEPENDENCIES
+    EXECUTABLES ${THERION} ${LOCH}
+    RESOLVED_DEPENDENCIES_VAR DLLS
+)
 
 file(MAKE_DIRECTORY ${DLLS_DIR})
 
 foreach(DLL ${DLLS})
-    gp_resolve_item("" ${DLL} "" "" RESOLVED)
-    message("Copying dependency: ${RESOLVED}")
-    file(COPY ${RESOLVED} DESTINATION ${DLLS_DIR})
+    message("Copying dependency: ${DLL}")
+    file(COPY ${DLL} DESTINATION ${DLLS_DIR})
 endforeach()
