655fe8f8e1fea176c9dab551a7b3e38ac33ca53f
Stacho Mudrák <stacho.mudrak@gmail.com>
2025-01-05 14:52:13 +0100

    Full name instead of name insertion.

--- thexpmap.cxx
+++ thexpmap.cxx
@@ -1709,8 +1709,8 @@ if (ENC_NEW.NFSS==0) {
                   SCRAPITEM->name = thexpmap_u2string(sscrap);
                   if (cs->name != nullptr)
                 	  SCRAPITEM->src_name = cs->name;
-                  if ((cs->fsptr != nullptr) and (cs->fsptr->name != nullptr))
-                	  SCRAPITEM->src_survey = cs->fsptr->name;
+                  if ((cs->fsptr != nullptr) and (cs->fsptr->full_name != nullptr))
+                	  SCRAPITEM->src_survey = cs->fsptr->full_name;
 
                   active_clr.set_color(this->layout->color_model, SCRAPITEM->col_scrap);
                   
