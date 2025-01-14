ea06ff67e91739f38a95cac82b1d672c6093a9bc
Martin Budaj <m.budaj@gmail.com>
2025-01-14 20:40:18 +0100

    fix loch crashing on Wayland on non-Debian based distros
    patch by Olly Betts

--- loch/lxGUI.cxx
+++ loch/lxGUI.cxx
@@ -73,7 +73,29 @@
 #endif
 
 
+#ifdef __WXGTK3__
+# if !(wxUSE_GLCANVAS_EGL-0)
+#  include <stdlib.h>
 
+// The GLX-based wxGLCanvas doesn't work under Wayland, and the code
+// segfaults: https://github.com/wxWidgets/wxWidgets/issues/17702
+//
+// Therefore we force X11 unless we're using the EGL-based wxGLCanvas
+// (which was added in wxWidgets 3.1.5 and hasn't been backported to
+// 3.0.x).
+//
+// Setting GDK_BACKEND=x11 is the recommended workaround, and it seems to
+// work to set it here.  GTK2 doesn't support Wayland, so doesn't need
+// this.
+struct ForceX11 {
+    ForceX11() {
+       setenv("GDK_BACKEND", "x11", 1);
+    }
+};
+static ForceX11 forcex11;
+
+# endif
+#endif
 
 ////////////////////////////////////////////////////////////////////////
 // Main frame
