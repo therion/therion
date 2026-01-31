fcc9e37d445a42a08baf36545c6c130aafb9f25a
Martin Budaj <m.budaj@gmail.com>
2026-01-31 16:31:30 +0100

    test with Proj 9.7.1

--- test_proj.sh
+++ test_proj.sh
@@ -25,7 +25,7 @@ while getopts "bi:mp:" opt; do
 done
 
 SRCPATH=$(dirname $BASH_SOURCE)
-PROJVER=${PROJVER:-6.3.1 6.3.2 7.0.1 7.2.1 8.0.0 8.2.1 9.0.0 9.7.0}
+PROJVER=${PROJVER:-6.3.1 6.3.2 7.0.1 7.2.1 8.0.0 8.2.1 9.0.0 9.7.1}
 
 URL=https://download.osgeo.org/proj/proj
 
