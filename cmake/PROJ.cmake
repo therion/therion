a4055a223e2226ecb6a7a3492ece05c37b06c4a5
Martin Budaj <m.budaj@gmail.com>
2026-05-26 19:29:35 +0200

    workaround for a min version check on windows
    otherwise we get an error:
    Could not find a configuration file for package PROJ that is compatible
      with requested version 8.0.0.
    
      The following configuration files were considered but not accepted:
    
        D:/a/_temp/msys64/clang64/lib/cmake/proj/proj-config.cmake, version: 9.8.0
          The version found is not compatible with the version requested.

--- cmake/PROJ.cmake
+++ cmake/PROJ.cmake
@@ -9,17 +9,19 @@ option(ENABLE_PROJ_CMAKE "Use CMake to find PROJ library." OFF)
 add_library(proj-interface INTERFACE)
 
 if (ENABLE_PROJ_CMAKE)
-    find_package(PROJ 8.0.0 REQUIRED)
+    find_package(PROJ REQUIRED)  # the version check can't be here if major versions differ
     target_link_libraries(proj-interface INTERFACE PROJ::proj)
 else()
     if (NOT PKG_CONFIG_FOUND)
         find_package(PkgConfig REQUIRED)
     endif()
-    pkg_check_modules(PROJ REQUIRED IMPORTED_TARGET "proj>=8.0.0")
+    pkg_check_modules(PROJ REQUIRED IMPORTED_TARGET proj)
     target_link_libraries(proj-interface INTERFACE PkgConfig::PROJ)
 endif()
 
 string(FIND ${PROJ_VERSION} "." MVER_SEP)
 string(SUBSTRING ${PROJ_VERSION} 0 ${MVER_SEP} PROJ_MVER)
-
+if (PROJ_VERSION VERSION_LESS 8.0.0)
+    message(FATAL_ERROR "Unsupported proj version: ${PROJ_VERSION}")
+endif()
 target_compile_definitions(proj-interface INTERFACE PROJ_VER=${PROJ_MVER})
